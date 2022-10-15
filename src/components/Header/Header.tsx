0c7cf01262b3cbf47baa1617d4cdd691e393744c
Pablo Puga <pablo.puga.git@gmail.com>
2022-10-15 12:55:55 +0200

    First work on the home page with the header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,26 @@
+import Image from 'next/image';
+
+import style from './Header.module.css';
+
+const Header = () => (
+    <header className={style.header}>
+        <div className={style['header-img-container']}>
+            <Image
+                src="/img/cabecera-mediana.jpg"
+                layout="responsive"
+                alt="Cabecera de la web"
+                width={1920}
+                height={1080}
+                priority={true}
+                style={{ zIndex: -50 }}
+                objectFit="cover"
+                objectPosition="center"
+            />
+        </div>
+        <div className="w-full px-2 sm:px-3 md:px-4 lg:px-0 flex flex-row justify-center">
+            <h1 className={style['header-title']}>Las Recetas de Pablo</h1>
+        </div>
+    </header>
+);
+
+export { Header };
