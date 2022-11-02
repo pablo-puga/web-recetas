d13e0f370931d27ef147056294d9f9ffa6fcacaa
Pablo Puga <pablo.puga.git@gmail.com>
2022-11-02 13:17:27 +0100

    Added a bit of spacing between the published date and the tags in the recipe list

--- src/components/CategoryList/InlineCategoryList.tsx
+++ src/components/CategoryList/InlineCategoryList.tsx
@@ -3,7 +3,7 @@ import { CategoryBadge } from '../CategoryBadge';
 import type { Category } from '../../types';
 
 const InlineCategoryList = ({ categories }: { categories: Category[] }) => (
-    <ul className="inline-flex flex-row flex-wrap">
+    <ul className="inline-flex flex-row flex-wrap mt-1">
         {categories.map((category) => (
             <li key={category.id} className="ml-3 first:ml-0">
                 <CategoryBadge category={category} />
