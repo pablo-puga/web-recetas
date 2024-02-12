3933b3afdc76e0bc826a3d520d243d4914abae58
Pablo Puga <pablo.puga.git@gmail.com>
2024-02-12 18:12:05 +0100

    Added lateral information panel to the Recipe page. Improved the general recipe layout. Style improvements.

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,11 +1,23 @@
 import { defineCollection, z } from 'astro:content';
 
+const capitalize = (str: string) =>
+    str
+        .split(' ')
+        .map((word) => {
+            const letters = word.split('');
+            letters[0] = letters[0].toLocaleUpperCase();
+            return letters.join('');
+        })
+        .join(' ');
+
 const recipesCollection = defineCollection({
     type: 'content',
     schema: ({ image }) =>
         z.object({
             title: z.string(),
-            categories: z.array(z.string()),
+            categories: z
+                .array(z.string())
+                .transform((categories) => categories.map(capitalize)),
             keywords: z.array(z.string()).optional(),
             published: z.boolean(),
             description: z.string().optional(),
